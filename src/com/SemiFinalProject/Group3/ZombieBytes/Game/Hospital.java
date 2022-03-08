fb044964acc6645b462d8c7eade80287fe31caaa
chuck8tenz <charleswill23@gmail.com>
2022-03-08 09:41:02 -0800

    Added layout for Location interface. Added Store, PoliceStation, Hospital, and School

---
+++ src/com/SemiFinalProject/Group3/ZombieBytes/Game/Hospital.java
@@ -0,0 +1,21 @@
+package com.SemiFinalProject.Group3.ZombieBytes.Game;
+
+import com.SemiFinalProject.Group3.ZombieBytes.Items.Items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hospital implements Location{
+    String name = null;
+    List<Items> placedItems = new ArrayList<>();
+
+    @Override
+    public void checkItems() {
+
+    }
+
+    @Override
+    public ArrayList<Items> pickUp() {
+        return null;
+    }
+}
---
+++ src/com/SemiFinalProject/Group3/ZombieBytes/Game/Location.java
@@ -0,0 +1,18 @@
+package com.SemiFinalProject.Group3.ZombieBytes.Game;
+
+import com.SemiFinalProject.Group3.ZombieBytes.Items.Items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Location {
+    //fields
+    String name = null;
+    List<Items> placedItems = new ArrayList<>();
+
+    //methods
+    public void checkItems();
+
+    public ArrayList<Items> pickUp();
+
+}
