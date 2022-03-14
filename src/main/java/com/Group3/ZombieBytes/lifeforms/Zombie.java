85f6d1a0d39e905dbe0620673ceba29fe0445d36
Emgear <emily.gear@snc.edu>
2022-03-14 10:11:52 -0500

    Added working attack method with hit and run away choices functional, #AB14

--- src/main/java/com/Group3/ZombieBytes/lifeforms/Zombie.java
+++ src/main/java/com/Group3/ZombieBytes/lifeforms/Zombie.java
@@ -1,69 +1,29 @@
 package com.Group3.ZombieBytes.lifeforms;
 
 public class Zombie {
-    private int ZombieHP;
-
     // Properties
-    private String zombieName = null;
+    private String zombieName;
     private String Zombies = null;
-//    String zombieHP = null;
-    private String zombieDescription = null;
-    public long zombieHP;
+    private String zombieDescription;
+    public static long zombieHP;
+    private static int newHealth;
 
     // constructors
     public Zombie(String Zombies, String zombieDescription, long zombieHP){
         this.zombieName = Zombies;
         this.zombieDescription = zombieDescription;
         this.zombieHP = zombieHP;
-
-//        getZombieName(zombieName);
-//        setZombies(Zombies);
-//        setZombieHP(zombieHP);
-
     }
-
 
     //methods
-    public String getZombieDescription() {
-        return zombieDescription;
-    }
-    public void setZombieDescription(String zombieDescription) {
-        this.zombieDescription = zombieDescription;
-    }
     public String getZombieName() {
         return zombieName;
     }
 
-    public void setZombieName(String zombieName) {
-        this.zombieName = zombieName;
+    public static void bite() {
+        Character.health = Character.health - 10;
+        System.out.println("The zombie bit you!");
     }
-
-//    public String getZombies() {
-//        return Zombies;
-//    }
-//
-//    public void setZombies(String Zombies) {
-//        this.Zombies = Zombies;
-//    }
-//    public String getZombieHP() {
-//        return zombieHP;
-//    }
-//
-//    public void setZombieHP(String zombieHP) {
-//        this.zombieHP = zombieHP;
-//    }
-
-//    public int bite() {
-//    if (difficulty == Level.EASY) {
-//        Character.health = Character.health - 10;
-//    } else if (difficulty == Level.MEDIUM) {
-//        Character.health = Character.health - 20;
-//    } else if (difficulty == Level.HARD) {
-//        Character.health = Character.health - 30;
-//    }
-//    return Character.health;
-//    }
-
 
     // to String
     public String toString(){
