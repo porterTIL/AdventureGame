484489ee91bd1baa54056ec0ce80f7ada7038e9d
mikebarrett19 <barrett.michaeljohn@gmail.com>
2022-03-22 09:12:16 -0500

    oops needed capital B Boolean AB#265

--- src/com/Group3/ZombieBytes/Util/JsonParser/ItemParser.java
+++ src/com/Group3/ZombieBytes/Util/JsonParser/ItemParser.java
@@ -43,7 +43,7 @@ public class ItemParser {
                     String type = (String) item.get("type");
                     String description = (String) item.get("Description");
                     String use = (String) item.get("use");
-                    Boolean visible = (boolean) item.get("visible");
+                    Boolean visible = (Boolean) item.get("visible");
                     if(visible==null) visible=true;
                     itemList.add(new Item(location, name, quantity, type, description, use, visible));
                 }
